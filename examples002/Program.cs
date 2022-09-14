84af34fcd857fbaa0d6ca9dcb29ecf6335875bec
Dzhon <evgenroo@gmail.com>
2022-09-15 01:28:17 +0300

    examples002

---
+++ examples002/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
